7de81512016ea5039b156792ac06d0a2cd0f2761
AMAnuMohan <anuamakv19@gmail.com>
2025-10-15 12:47:45 +0100

    palywright - Cucumber BDD framwork initialization

---
+++ tests/hooks/hooks.ts
@@ -0,0 +1,69 @@
+import { Before, AfterStep, After, setDefaultTimeout } from '@cucumber/cucumber';
+import { Browser, BrowserContext, Page, chromium } from 'playwright';
+import { setWorldConstructor, World } from '@cucumber/cucumber';
+import { CommonActions } from '../utils/commonActions';
+
+setDefaultTimeout(100 * 1000);
+class CustomWorld extends World {
+  browser!: Browser;
+  context!: BrowserContext;
+  page!: Page;
+
+  common!: CommonActions;
+
+ /**
+ * Initialises the Playwright browser, context, and page instances for test execution.
+ */
+  async initBrowser() {
+    this.browser = await chromium.launch({
+      headless: false,
+      channel: 'chrome',
+      args: ["--start-maximized"],
+    })
+    this.context = await this.browser.newContext({
+      viewport: null,
+    });
+    this.page = await this.context.newPage()
+
+    this.common = new CommonActions(this.page);
+  }
+
+  /**
+ * Closes the Playwright page and browser instances to clean up resources after test execution.
+ */
+  async closeBrowser() {
+    if (this.page) {
+      await this.page.close();
+    }
+    if (this.browser) {
+      await this.browser.close();
+    }
+  }
+
+}
+
+/**
+ * Registers the custom World constructor for Cucumber.
+ */
+setWorldConstructor(CustomWorld);
+
+/*
+* Before each scenario, initialise a fresh browser, context, and page
+*/
+Before(async function () {
+  await this.initBrowser();
+})
+
+/*
+* After each step, take a screenshot for reporting/debugging purposes
+*/
+AfterStep(async function () {
+  await this.page.screenshot({ path: `screenshot/screenshot ${new Date().getMilliseconds()}.png` });
+})
+
+/*
+* After each scenario, close the browser and page to clean up resources
+*/
+After(async function () {
+  await this.closeBrowser();
+})
